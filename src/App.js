f89b1017f693063f3fb1b1e39febd99c314bf63e
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-10-08 13:45:53 -0300

    Remove Counter example from redux template and clean App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { Login } from "./features/user/Login";
 
 function App() {
   return (
     <div>
+      <h1>PyRobots</h1>
       <Login />
     </div>
   );
