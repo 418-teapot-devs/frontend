e5e8676a7c5b2f98e7e2448b7d75f4e10cc70fa2
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-10-08 12:32:22 -0300

    Add basic component to test userSlice

---
+++ src/features/user/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  loginAsync,
+  logout,
+  selectToken,
+  selectLoginLoading,
+  selectLoginErrors,
+} from "./userSlice";
+
+export function Login() {
+  const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+  const loading = useSelector(selectLoginLoading);
+  const errors = useSelector(selectLoginErrors);
+
+  return (
+    <div>
+      <span>
+        <button
+          disabled={loading}
+          onClick={() => dispatch(loginAsync("user", "pass"))}
+        >
+          Login
+        </button>
+        <button disabled={loading} onClick={() => dispatch(logout())}>
+          Logout
+        </button>
+      </span>
+      {loading ? <p>Loading...</p> : <img src={token} height="500px" />}      
+    </div>
+  );
+}
