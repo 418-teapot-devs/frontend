ffd543a09865d307007f6bc4367a85f9335bc9e3
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2022-10-11 15:35:44 -0300

    form fields without error checking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react"
+import { Login } from "./features/user/Login"
+import { Profile } from "./features/user/Profile"
+import { CreateMatch } from "./features/matches/CreateMatch"
 import { IniciatedMatches } from "./features/matches/IniciatedMatches"
 import { iniciated } from "./features/matches/api/iniciated"
 
@@ -10,7 +13,14 @@ function App() {
   return (
     <div style={divStyle}>
       <h1>PyRobots</h1>
-      <IniciatedMatches matches={iniciated}/>
+      <Grid container justifyContent="center" spacing={2}>
+        <Grid item xs={12} md={6} lg={3}>
+          <Login />
+        </Grid>
+        <Grid item xs={12} md={6} lg={3}>
+          <Profile />
+        </Grid>
+      </Grid>
     </div>
   )
 }
