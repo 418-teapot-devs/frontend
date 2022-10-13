42763b01657168075bda4eab8fdd292583de0644
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-10-13 12:06:30 -0300

    Add zoom and pan to Board

--- src/features/simulation/Board.js
+++ src/features/simulation/Board.js
@@ -1,13 +1,103 @@
 import React from "react"
-import { Stage, Layer, Rect, Circle, Image } from "react-konva";
-import useImage from "use-image";
+import { Stage, Layer, Rect, Circle, Image } from "react-konva"
+import useImage from "use-image"
 
+let delta = {
+  x: 0,
+  y: 0
+}
+let isMoving = false
 
 function Board() {
   const [image] = useImage("https://konvajs.org/assets/lion.png")
+  const stageRef = React.useRef(null)
+
+
+  const handleOnWheel = (e) => {
+    e.evt.preventDefault()
+
+    let scaleBy = 1.08
+
+    const stage = stageRef.current
+
+    let oldScale = stage.scaleX()
+    let pointer = stage.getPointerPosition()
+
+    let mousePointTo = {
+      x: (pointer.x - stage.x()) / oldScale,
+      y: (pointer.y - stage.y()) / oldScale,
+    }
+
+    let direction = e.evt.deltaY > 0 ? -1 : 1
+
+    if (e.evt.ctrlKey) {
+      scaleBy = 1.16
+    }
+
+    let newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy
+
+    stage.scale({ x: newScale, y: newScale })
+
+    let newPos = {
+      x: pointer.x - mousePointTo.x * newScale,
+      y: pointer.y - mousePointTo.y * newScale,
+    }
+    stage.position(newPos)
+  }
+
+  const handleOnMouseMove = (e) => {
+    const stage = stageRef.current
+
+    let scale = stage.scaleX()
+    let pointer = stage.getPointerPosition()
+
+    console.log({
+      pointer_x: pointer.x,
+      pointer_y: pointer.y,
+      stage_x: stage.x(),
+      stage_y: stage.y(),
+    })
+
+    if (e.evt.buttons === 1) {
+      if (!isMoving) {
+        isMoving = true
+
+        const stage_x = stage.x()
+        const stage_y = stage.y()
+
+        delta.x = Math.abs(pointer.x - stage.x())
+        delta.y = Math.abs(pointer.y - stage.y())
+
+        if (stage_x < pointer.x) {
+          delta.x = -delta.x
+        }
+
+        if (stage_y < pointer.y) {
+          delta.y = -delta.y
+        }
+      }
+
+      let newPos = {
+        x: pointer.x + delta.x,
+        y: pointer.y + delta.y,
+      }
+  
+      stage.position(newPos)        
+    } else {
+      isMoving = false
+      delta.x = 0
+      delta.y = 0
+    }
+  };
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Stage
+      ref={stageRef}
+      width={1000}
+      height={1000}
+      onWheel={handleOnWheel}
+      onMouseMove={handleOnMouseMove}
+    >
       <Layer>
         <Image width={50} height={50} image={image} />
       </Layer>
