4d411e0a97d4543761f9f163ddeefa0e66386498
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2022-10-13 12:22:02 -0300

    App that renders list of created matches

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from "react"
-import { CreateMatch } from "./features/matches/CreateMatch"
+import { CreatedMatches } from "./features/matches/CreatedMatches"
 
-const robots = [{name: "Robot"}, {name: "Robot1"}]
 function App() {
   return (
     <div>
       <h1>PyRobots</h1>
-      <Grid container justifyContent="center" spacing={2}>
-        <Grid item xs={12} md={6} lg={3}>
-          <CreateMatch userRobots={robots}/>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-
-        </Grid>
-      </Grid>
+      <CreatedMatches />
     </div>
   )
 }
