106f023945d31206fe075d2fe7259aba859a3ff3
annaaimeri <64292241+annaaimeri@users.noreply.github.com>
2022-10-14 10:26:08 -0300

    Connect w/backend

--- src/features/user/api/register.js
+++ src/features/user/api/register.js
@@ -1,24 +1,19 @@
 // // A mock function to mimic making an async request for data
   export const register = (values) => {
     const params = [
-      `username=${values.username}`,
-      `e_mail=${values.email}`,
-      `password=${values.password}`
+      `username=${encodeURIComponent(values.username)}`,
+      `e_mail=${encodeURIComponent(values.email)}`,
+      `password=${encodeURIComponent(values.password)}`
     ].join('&');
 
-    // return fetch('http://127.0.0.1:8000/users/?' + new URLSearchParams({
-    return fetch(`http://127.0.0.1:8000/users/?${params}`,
-    //({
-    //   username: values.username,
-    //   email: values.email,
-    //   password: values.password
-    // }), 
+    console.log(params);
+
+    return fetch(`http://localhost:8000/users/?${params}`,
     {
-      protocol: 'http:',
       method: "POST",
       headers: { 
-        "accept": "application/json", 
-        "Content-Type": "multipart/form-data" 
+        "accept": 'application/json', 
+        "Content-Type": 'multipart/form-data' 
       }
     })
   }
