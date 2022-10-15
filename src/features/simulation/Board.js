2722f9699bb15913c156d84dbfac4c9c466cd07d
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-10-15 11:32:29 -0300

    Ran prettier

--- src/features/simulation/Board.js
+++ src/features/simulation/Board.js
@@ -6,66 +6,49 @@ import Robot from "./Robot"
 const initialRobots = [
   {
     x: Math.random() * 1000,
-    y: Math.random() * 1000
+    y: Math.random() * 1000,
   },
   {
     x: Math.random() * 1000,
-    y: Math.random() * 1000
+    y: Math.random() * 1000,
   },
   {
     x: Math.random() * 1000,
-    y: Math.random() * 1000
+    y: Math.random() * 1000,
   },
   {
     x: Math.random() * 1000,
-    y: Math.random() * 1000
+    y: Math.random() * 1000,
   },
   {
     x: Math.random() * 1000,
-    y: Math.random() * 1000
-  }
+    y: Math.random() * 1000,
+  },
 ]
 
 const Board = () => {
   const [robots, setRobots] = useState(initialRobots)
 
   const robotsRandomPositions = () => {
-    setRobots(robots.map((_) => ({
-      x: Math.random() * 1000,
-      y: Math.random() * 1000
-    })))
+    setRobots(
+      robots.map((_) => ({
+        x: Math.random() * 1000,
+        y: Math.random() * 1000,
+      }))
+    )
   }
 
   return (
     <React.Fragment>
-      <button
-        onClick={() => robotsRandomPositions()}
-      >
+      <button onClick={() => robotsRandomPositions()}>
         Move rect to random Position
       </button>
-      <CustomStage
-        width={1000}
-        height={1000}
-      >
+      <CustomStage width={1000} height={1000}>
         <Layer>
-          <Rect
-            x={0}
-            y={0}
-            width={1000}
-            height={1000}
-            fill="grey"
-          />
-          {
-            robots.map((robot) => (
-              <Robot
-                position={robot}
-                width={30}
-                height={30}
-                fill="red"
-              />
-            ))
-          }
-          
+          <Rect x={0} y={0} width={1000} height={1000} fill="grey" />
+          {robots.map((robot) => (
+            <Robot position={robot} width={20} height={20} fill="red" />
+          ))}
         </Layer>
       </CustomStage>
     </React.Fragment>
--- src/features/simulation/Robot.js
+++ src/features/simulation/Robot.js
@@ -1,20 +1,14 @@
 import React, { useEffect } from "react"
 import { Rect } from "react-konva"
 
-
 const Robot = ({ position, ...props }) => {
   const rectRef = React.useRef(null)
   useEffect(() => {
     const rect = rectRef.current
     rect.to({ ...position, duration: 0.5 })
-  });
+  })
 
-  return (
-    <Rect
-      ref={rectRef}
-      {...props}
-    />
-  )
+  return <Rect ref={rectRef} {...props} />
 }
 
 export default Robot
