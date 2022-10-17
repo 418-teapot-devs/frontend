833198c8cb97eb8451a8a2e60bdf7443b59cf257
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-10-17 17:03:43 -0300

    Add SideBar

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,13 +1,16 @@
-import { useAuth } from "../hooks/useAuth"
+import { Box, Stack } from "@mui/material"
+import { Outlet } from "react-router-dom"
+import SideBar from "./SideBar"
 
 export const Root = () => {
-  const { user, logout } = useAuth()
-
   return (
-    <div>
-      <h1>PyRobots</h1>
-      <p>{user.token}</p>
-      <button onClick={logout}>Cerrar Sesión</button>
-    </div>
+    <Stack sx={{ height: "100% " }} direction="row">
+      <Box sx={{ height: "100vh", padding: 1 }}>
+        <SideBar />
+      </Box>
+      <Box sx={{ padding: 1 }}>
+        <Outlet />
+      </Box>
+    </Stack>
   )
 }
