7ac3ba519994a7d67affdcc654acf25dad4305c4
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2022-10-17 20:18:13 -0300

    Login and Resister in /login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { useRoutes } from "react-router-dom"
-import { Login } from "./features/user/Login"
 import { Root } from "./routes/Root"
 import { ProtectedRoute } from "./routes/ProtectedRoute"
 import { CreateMatch } from "./features/matches/CreateMatch"
+import  LoginAndRegister  from "./routes/LoginAndRegister"
 
 const App = () => {
   const routes = useRoutes([
@@ -35,11 +35,7 @@ const App = () => {
     },
     {
       path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/creatematch",
-      element: <CreateMatch userRobots={[{ name: "Robot1" }]} />,
+      element: <LoginAndRegister />,
     },
   ])
 
