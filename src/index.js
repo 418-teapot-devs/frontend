50ec3f8565fc399062ce895c86c137ee51e0e292
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-10-17 00:13:43 -0300

    Add custom auth hook and provider

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { Provider } from "react-redux"
-import { store } from "./app/store"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-import "./index.css"
+import { BrowserRouter } from "react-router-dom"
+import { AuthProvider } from "./hooks/useAuth"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
 
