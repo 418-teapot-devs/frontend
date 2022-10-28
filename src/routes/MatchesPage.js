2f2655a427f7051a59ad2946304fb3c32f13ff52
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2022-10-28 16:53:06 -0300

    Reorder in Matches page and tested

--- src/routes/MatchesPage.js
+++ src/routes/MatchesPage.js
@@ -1,23 +1,50 @@
 import { Grid } from "@mui/material"
 import React from "react"
-import { PublicMatches, CreatedMatches } from "../features/matches/list/Matches"
+import { PublicMatches, CreatedMatches, StartedMatches, JoinedMatches } from "../features/matches/list/Matches"
 import { CreateMatch } from "../features/matches/create/CreateMatch"
+import { Box } from "@mui/system"
 
 export const MatchesPage = () => {
   return (
     <React.Fragment>
-      <h2>Partidas</h2>
-      <Grid container>
-        <Grid item xs={12} md={6}>
-          <CreateMatch />
+      <h1>Partidas</h1>
+      <Grid container >
+
+        <Grid item  md={6}>
+          <Box sx={{padding: 3 }} >
+            <h2>Partidas Propias</h2>
+            <Box
+              sx={{width:"100%", height:300, overflow:"scroll"}}
+              data-testid="created-matches">
+              <CreatedMatches  />
+            </Box>
+          </Box>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <CreatedMatches />
+
+        <Grid item md={6} >
+        <Box sx={{padding: 3}}>
+          <h2>Partidas Compartidas</h2>
+          <Box
+            sx={{width:"100%", height:300, overflow:"scroll"}}
+            data-testid="joined-matches"
+          >
+              <JoinedMatches />
+          </Box>
+        </Box>
         </Grid>
-        <Grid item xs={12}>
-          <PublicMatches />
-        </Grid>
+      
       </Grid>
+      <Box
+        data-testid="public-matches"
+      >
+        <h2>Partidas Públicas</h2>
+        <PublicMatches/>
+      </Box>
+      <Box
+        data-testid="started-matches">
+        <h2>Historial de partidas</h2>
+        <StartedMatches/>
+      </Box>
     </React.Fragment>
   )
 }
