ff308734e81a4127d3cf3ce2e178f46506a8e7ac
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-10-31 15:48:00 -0300

    Revert old SideBar

--- src/features/simulation/Simulation.js
+++ src/features/simulation/Simulation.js
@@ -1,4 +1,4 @@
-import { Button, Stack } from "@mui/material"
+import { Button, Paper, Stack } from "@mui/material"
 import Slider, { sliderClasses } from "@mui/material/Slider"
 import React, { useEffect, useRef, useState } from "react"
 import Board from "./Board"
@@ -30,48 +30,50 @@ const Simulation = ({ robots, rounds }) => {
   }))
 
   return (
-    <Stack
-      sx={{ height: "calc(100vh - 16px)" }}
-      justifyContent="space-between"
-      alignItems="center"
-    >
-      <Stack direction="row" spacing={1} sx={{ maxHeight: "90%" }}>
-        <Board robots={roundData} />
-        <Stack spacing={1}>
-          {robotsWithId.map((robot, i) => (
-            <RobotStatus
-              key={i}
-              name={robot.name}
-              {...rounds[round].robots[robot.robot_id]}
-            />
-          ))}
+    <Paper sx={{ padding: 2}} variant="outlined">
+      <Stack
+        sx={{ height: "calc(100vh - 50px)" }}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Stack direction="row" spacing={1} sx={{ maxHeight: "90%" }}>
+          <Board robots={roundData} />
+          <Stack spacing={1}>
+            {robotsWithId.map((robot, i) => (
+              <RobotStatus
+                key={i}
+                name={robot.name}
+                {...rounds[round].robots[robot.robot_id]}
+              />
+            ))}
+          </Stack>
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ paddingRight: 2, width: "100%" }}
+        >
+          <Button variant="outlined" onClick={() => setPaused(!paused)}>
+            {paused ? "Play" : "Pause"}
+          </Button>
+          <Slider
+            sx={{
+              [`& .${sliderClasses.track}`]: {
+                transition: "none",
+              },
+              [`& .${sliderClasses.thumb}`]: {
+                transition: "none",
+              },
+            }}
+            defaultValue={0}
+            value={round}
+            min={0}
+            max={rounds.length - 1}
+            onChange={(e, newValue) => setRound(newValue)}
+          />
         </Stack>
       </Stack>
-      <Stack
-        direction="row"
-        spacing={2}
-        sx={{ paddingRight: 2, width: "100%" }}
-      >
-        <Button variant="outlined" onClick={() => setPaused(!paused)}>
-          {paused ? "Play" : "Pause"}
-        </Button>
-        <Slider
-          sx={{
-            [`& .${sliderClasses.track}`]: {
-              transition: "none",
-            },
-            [`& .${sliderClasses.thumb}`]: {
-              transition: "none",
-            },
-          }}
-          defaultValue={0}
-          value={round}
-          min={0}
-          max={rounds.length - 1}
-          onChange={(e, newValue) => setRound(newValue)}
-        />
-      </Stack>
-    </Stack>
+    </Paper>
   )
 }
 
--- src/routes/SideBar.js
+++ src/routes/SideBar.js
@@ -1,9 +1,8 @@
 import {
+  Box,
   List,
   ListItem,
   ListItemButton,
-  ListItemIcon,
-  ListItemText,
   Paper,
   Stack,
   Tooltip,
@@ -49,13 +48,14 @@ const extraOptions = [
   },
 ]
 
+export const SIDEBAR_WIDTH = 64
+
 const SideBarItem = (props) => {
   return (
     <ListItem disablePadding>
-      <ListItemButton onClick={props.onClick}>
-        <ListItemIcon>{props.icon}</ListItemIcon>
-        <ListItemText>{props.title}</ListItemText>
-      </ListItemButton>
+      <Tooltip title={props.title} placement="right">
+        <ListItemButton onClick={props.onClick}>{props.icon}</ListItemButton>
+      </Tooltip>
     </ListItem>
   )
 }
@@ -65,37 +65,42 @@ export const SideBar = () => {
   const navigate = useNavigate()
 
   return (
-    <Paper sx={{ height: "100%" }} variant="outlined">
-      <Stack
-        direction="column"
-        justifyContent="space-between"
-        sx={{ height: "100%" }}
+    <Box sx={{ padding: 1, position: "fixed", top: 0, left: 0 }}>
+      <Paper
+        sx={{ height: "calc(100vh - 16px)", width: `${SIDEBAR_WIDTH}px` }}
+        variant="outlined"
       >
-        <List>
-          {mainOptions.map((option) => (
+        <Stack
+          direction="column"
+          justifyContent="space-between"
+          sx={{ height: "100%" }}
+        >
+          <List>
+            {mainOptions.map((option) => (
+              <SideBarItem
+                key={option.title}
+                {...option}
+                onClick={() => navigate(option.to)}
+              />
+            ))}
+          </List>
+          <List>
+            {extraOptions.map((option) => (
+              <SideBarItem
+                key={option.title}
+                {...option}
+                onClick={() => navigate(option.to)}
+              />
+            ))}
             <SideBarItem
-              key={option.title}
-              {...option}
-              onClick={() => navigate(option.to)}
+              title="Cerrar Sesión"
+              icon={<LogoutOutlinedIcon />}
+              onClick={logout}
             />
-          ))}
-        </List>
-        <List>
-          {extraOptions.map((option) => (
-            <SideBarItem
-              key={option.title}
-              {...option}
-              onClick={() => navigate(option.to)}
-            />
-          ))}
-          <SideBarItem
-            title="Cerrar Sesión"
-            icon={<LogoutOutlinedIcon />}
-            onClick={logout}
-          />
-        </List>
-      </Stack>
-    </Paper>
+          </List>
+        </Stack>
+      </Paper>
+    </Box>
   )
 }
 
