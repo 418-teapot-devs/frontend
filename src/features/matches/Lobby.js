4926ed108ac3c4f58b5fda6187d2fd25adfbdc57
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-10-31 11:39:46 -0300

    Changes to Lobby

--- src/features/matches/Lobby.js
+++ src/features/matches/Lobby.js
@@ -1,14 +1,9 @@
 import { React, useEffect, useState, Redirect } from "react"
 import { useAuth } from "../../hooks/useAuth"
-import AddIcon from "@mui/icons-material/Add"
-import { createTheme } from "@mui/material/styles"
-import { mockmatch } from "./api/mockmatch"
 import { grey } from "@mui/material/colors"
 import { PersonOutlined, LockOutlined } from "@mui/icons-material"
 import {
   Avatar,
-  AvatarGroup,
-  Box,
   Card,
   CardContent,
   Divider,
@@ -16,138 +11,115 @@ import {
   Tooltip,
   Typography,
   Stack,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  CardActions,
+  Collapse,
+  Slide,
 } from "@mui/material"
-import { ThemeProvider } from "@emotion/react"
+import { TransitionGroup } from "react-transition-group"
 
-var matchCardStyle = {
-  display: "block",
-  transitionDuration: "0.3s",
-  height: "30vw",
-  width: "50vw",
-  overflow: "scroll",
-}
-
-var theme = createTheme({
-  typography: {
-    htmlFontSize: 11,
-  },
-})
-
-export const Lobby = () => {
-  const missing_robots = mockmatch.max_players - mockmatch.robots.length
-
-  //   const [loading, setLoading] = useState(false)
-  //   const [match, setMatch] = useState([])
-  //   const { user } = useAuth()
-
-  //   useEffect(() => {
-  //     const fetchMatch = async () => {
-  //       const response = await getMatch(user.token, props.match.params.id)
-
-  //       switch (response.status) {
-  //         case 200:
-  //           const body = await response.json()
-  //           setMatch(body)
-  //           setLoading(false)
-  //           break
-  //         default:
-  //           setLoading(false)
-  //           break
-  //       }
-  //     }
-
-  //     fetchMatch()
-  //   }, [user.token])
+export const Lobby = ({ match }) => {
+  const { user } = useAuth()
+  const missing_robots = match.max_players - match.robots.length
 
   return (
-    <Card style={matchCardStyle}>
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h4"
-          component="div"
-          textAlign="center"
-        >
-          {mockmatch.name}{" "}
-          {mockmatch.is_private && (
-            <Tooltip title="This match is private" placement="top-start" arrow>
-              <LockOutlined color="enabled" />
-            </Tooltip>
-          )}
-        </Typography>
-        <Grid container spacing={2} justifyContent="space-between">
-          <Grid container item xs={2} direction="column">
-            <Box display="inline-block">
-              <CardContent>
-                <Stack
-                  direction="row"
-                  alignItems="flex-end"
-                  justifyContent="space-between"
-                  sx={{ mb: 2 }}
+    <Grid container justifyContent="center">
+      <Grid item xs={12} md={8} lg={6}>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h4"
+              component="div"
+              textAlign="center"
+            >
+              {match.name}
+              {""}
+              {match.is_private && (
+                <Tooltip
+                  title="This match is private"
+                  placement="top-start"
+                  arrow
                 >
-                  <Stack direction="column" alignItems="left" noWrap>
-                    <ThemeProvider theme={theme}>
-                      <Typography variant="overline" margin={0} noWrap>
-                        Host: {mockmatch.username}
-                      </Typography>
-                      <Divider />
-                      <Typography variant="overline" margin={0} noWrap>
-                        Min. players: {mockmatch.min_players}
-                      </Typography>
-                      <Divider />
-                      <Typography variant="overline" margin={0} noWrap>
-                        Max. players: {mockmatch.max_players}
-                      </Typography>
-                      <Divider />
-                      <Typography variant="overline" margin={0} noWrap>
-                        #Games: {mockmatch.games}
-                      </Typography>
-                      <Divider />
-                      <Typography variant="overline" margin={0} noWrap>
-                        #Rounds: {mockmatch.rounds}
-                      </Typography>
-                    </ThemeProvider>
-                  </Stack>
-                </Stack>
-              </CardContent>
-            </Box>
-            <Grid />
-            <Grid container item xs={10} direction="column">
-              <AvatarGroup max={4}>
-                {mockmatch.robots.map((robot, index) => (
-                  <Tooltip
-                    title={`${robot.name}, @${robot.username}`}
-                    placement="top-start"
-                    arrow
-                    key={index}
-                  >
-                    <Avatar
-                      key={index}
-                      src={robot.avatar_url}
-                      sx={{ minWidth: "70px", minHeight: "70px" }}
-                    />
-                  </Tooltip>
-                ))}
-                {Array.from({ length: missing_robots }, (_, i) => (
-                  <Avatar
-                    key={i}
-                    sx={{
-                      bgcolor: "white",
-                      outline: "1.5px solid",
-                      outlineColor: grey[400],
-                      outlineOffset: "-4px",
-                      minWidth: "70px",
-                      minHeight: "70px",
-                    }}
-                  >
-                    <PersonOutlined sx={{ color: grey[400] }} />
-                  </Avatar>
-                ))}
-              </AvatarGroup>
-            </Grid>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+                  <LockOutlined color="enabled" />
+                </Tooltip>
+              )}
+            </Typography>
+
+            <Stack spacing={2}>
+              <Stack>
+                <Typography variant="overline" margin={0} noWrap>
+                  Host: {match.host.username}
+                </Typography>
+                <Divider />
+                <Typography variant="overline" margin={0} noWrap>
+                  Min. players: {match.min_players}
+                </Typography>
+                <Divider />
+                <Typography variant="overline" margin={0} noWrap>
+                  Max. players: {match.max_players}
+                </Typography>
+                <Divider />
+                <Typography variant="overline" margin={0} noWrap>
+                  #Games: {match.games}
+                </Typography>
+                <Divider />
+                <Typography variant="overline" margin={0} noWrap>
+                  #Rounds: {match.rounds}
+                </Typography>
+              </Stack>
+              <Stack>
+                <Typography>Robots:</Typography>
+                <List>
+                  <TransitionGroup>
+                    {match.robots.map((robot) => (
+                      <Slide
+                        key={robot.username + robot.name}
+                        direction="right"
+                      >
+                        <ListItem>
+                          <ListItemAvatar>
+                            <Avatar src={robot.avatar_url} />
+                          </ListItemAvatar>
+                          <ListItemText>
+                            <Typography
+                              variant="body1"
+                            >
+                              {robot.name}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                              @{robot.username}
+                            </Typography>
+                          </ListItemText>
+                        </ListItem>
+                      </Slide>
+                    ))}
+                  </TransitionGroup>
+                  {Array.from({ length: missing_robots }, (_, i) => (
+                    <ListItem key={i}>
+                      <ListItemAvatar>
+                        <Avatar
+                          sx={{
+                            bgcolor: "white",
+                            outline: "1.5px solid",
+                            outlineColor: grey[400],
+                          }}
+                        >
+                          <PersonOutlined sx={{ color: grey[400] }} />
+                        </Avatar>
+                      </ListItemAvatar>
+                    </ListItem>
+                  ))}
+                </List>
+              </Stack>
+            </Stack>
+          </CardContent>
+          <CardActions>{match.host.username === user.username}</CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
