145612c2268c0824276f81915e6f297950717009
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-11-02 11:04:19 -0300

    Add field array with no validation

--- src/features/simulation/SimulationForm.js
+++ src/features/simulation/SimulationForm.js
@@ -5,17 +5,18 @@ import {
   CardActions,
   CardContent,
   Grid,
+  IconButton,
   Stack,
   TextField,
   Typography,
 } from "@mui/material"
 import { RobotsSelect } from "../robots/RobotsSelect"
-import { useFormik } from "formik"
+import { Field, FieldArray, Form, Formik, useFormik } from "formik"
+import { DeleteOutlined } from "@mui/icons-material"
 
 const validationSchema = () =>
   yup.object({
-    robots: yup
-      .array()
+    robots: yup.array().of(yup.number())
       .required("Los robots son obligatorios")
       .min(1, "El mínimo de robots es 1")
       .max(4, "El máximo de robots es 4"),
@@ -30,7 +31,7 @@ const validationSchema = () =>
 const SimulationForm = ({ onSubmit }) => {
   const formik = useFormik({
     initialValues: {
-      robots: [],
+      robots: [""],
       rounds: 999,
     },
     validationSchema: validationSchema,
@@ -41,40 +42,80 @@ const SimulationForm = ({ onSubmit }) => {
     <Grid container justifyContent="center">
       <Grid item xs={12} md={6} lg={4}>
         <Card variant="outlined">
-          <form onSubmit={formik.handleSubmit}>
-            <CardContent>
-              <Stack spacing={2} justifyContent="center" textAlign="center">
-                <Typography variant="h5" gutterBottom>
-                  Simulación
-                </Typography>
-                <TextField
-                  fullWidth
-                  id="rounds"
-                  name="rounds"
-                  label="Cantidad de rondas *"
-                  type="number"
-                  value={formik.values.rounds}
-                  onChange={formik.handleChange}
-                  error={formik.touched.rounds && Boolean(formik.errors.rounds)}
-                  helperText={formik.touched.rounds && formik.errors.rounds}
-                />
-                <RobotsSelect
-                  value={formik.values.robots}
-                  multiple
-                  name="robots"
-                  onChange={formik.handleChange}
-                  label="Elegir Robot *"
-                  error={formik.touched.robots && Boolean(formik.errors.robots)}
-                />
-                <Typography color="error" variant="caption">{formik.errors.robots}</Typography>
-              </Stack>
-            </CardContent>
-            <CardActions>
-              <Button type="submit" fullWidth variant="contained">
-                Iniciar Simulación
-              </Button>
-            </CardActions>
-          </form>
+          <Formik
+            initialValues={{
+              robots: [""],
+              rounds: 999,
+            }}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+            render={({ values }) => (
+              <Form>
+                <CardContent>
+                  <Stack spacing={2} justifyContent="center" textAlign="center">
+                    <Typography variant="h5" gutterBottom>
+                      Simulación
+                    </Typography>
+                    <TextField
+                      fullWidth
+                      id="rounds"
+                      name="rounds"
+                      label="Cantidad de rondas *"
+                      type="number"
+                      value={formik.values.rounds}
+                      onChange={formik.handleChange}
+                      error={
+                        formik.touched.rounds && Boolean(formik.errors.rounds)
+                      }
+                      helperText={formik.touched.rounds && formik.errors.rounds}
+                    />
+                    <FieldArray
+                      name="robots"
+                      render={(arrayHelpers) => (
+                        <Stack spacing={2}>
+                          {values.robots.map((robot, index) => (
+                            <Stack direction="row" key={index}>
+                              <Field name={`robots.${index}`}>
+                                {({ field }) => (
+                                  <RobotsSelect
+                                    value={field.value}
+                                    name={field.name}
+                                    onChange={field.onChange}
+                                    label="Elegir Robot *"
+                                  />
+                                )}
+                              </Field>
+                              {values.robots.length > 1 && (
+                                <IconButton
+                                  onClick={() => arrayHelpers.remove(index)}
+                                >
+                                  <DeleteOutlined />
+                                </IconButton>
+                              )}
+                            </Stack>
+                          ))}
+                          {values.robots.length < 4 && (
+                            <Button onClick={() => arrayHelpers.push("")}>
+                              Añadir un robot
+                            </Button>
+                          )}
+                        </Stack>
+                      )}
+                    />
+
+                    <Typography color="error" variant="caption">
+                      {formik.errors.robots}
+                    </Typography>
+                  </Stack>
+                </CardContent>
+                <CardActions>
+                  <Button type="submit" fullWidth variant="contained">
+                    Iniciar Simulación
+                  </Button>
+                </CardActions>
+              </Form>
+            )}
+          ></Formik>
         </Card>
       </Grid>
     </Grid>
