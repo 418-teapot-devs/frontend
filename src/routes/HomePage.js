7019f707bcb2a79df9002bad8f4a30aac25a2fd7
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2022-11-18 10:27:36 -0300

    Commented deprecated Image component

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,16 +1,16 @@
 import React from "react"
-import { Image } from "mui-image"
+// import { Image } from "mui-image"
 import { Box } from "@mui/system"
 
 export const Home = () => {
   return (
     <Box sx={{ height: "90vh", padding: 6 }}>
-    <Image
+    {/* <Image
       src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b0d5b32f-7cf8-4974-9217-2a23344d235b/d82p4kv-90806bf5-6769-405b-ad9a-5852b612c5e7.jpg/v1/fill/w_894,h_894,q_70,strp/sci_fi_halloween_by_sempronski_d82p4kv-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcL2IwZDViMzJmLTdjZjgtNDk3NC05MjE3LTJhMjMzNDRkMjM1YlwvZDgycDRrdi05MDgwNmJmNS02NzY5LTQwNWItYWQ5YS01ODUyYjYxMmM1ZTcuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.Ts-z8W_VcQKxPlaafIz7GdtlVYAWHlIT3H6ozwsb1-M"
       alt="Logo"
       showLoading
 
-    />
+    /> */}
     </Box>
   )
 }
