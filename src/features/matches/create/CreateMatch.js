554707bb0a766328c37402bdadfcfe6f3e9dd3cd
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2022-11-18 09:38:25 -0300

    success message with auto hide

--- src/features/matches/create/CreateMatch.js
+++ src/features/matches/create/CreateMatch.js
@@ -18,6 +18,7 @@ import {
   Stack,
   Alert,
   AlertTitle,
+  Grid
 } from "@mui/material"
 
 const validationSchema = () =>
@@ -113,142 +114,152 @@ export const CreateMatch = () => {
   })
 
   return (
-    <Card variant="outlined">
-      <form onSubmit={formik.handleSubmit}>
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            textAlign="center"
-          >
-            Crear Partida
-          </Typography>
-          <Stack spacing={2}>
-            <TextField
-              fullWidth
-              id="name"
-              name="name"
-              label="Nombre de la partida*"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              error={formik.touched.name && Boolean(formik.errors.name)}
-              helperText={formik.touched.name && formik.errors.name}
-            />
-            <TextField
-              fullWidth
-              id="min_players"
-              name="min_players"
-              label="Cantidad mínima de jugadores*"
-              type="number"
-              value={formik.values.min_players}
-              onChange={formik.handleChange}
-              error={
-                formik.touched.min_players && Boolean(formik.errors.min_players)
-              }
-              helperText={
-                formik.touched.min_players && formik.errors.min_players
-              }
-            />
-            <TextField
-              fullWidth
-              id="max_players"
-              name="max_players"
-              label="Cantidad máxima de jugadores*"
-              type="number"
-              value={formik.values.max_players}
-              onChange={formik.handleChange}
-              error={
-                formik.touched.max_players && Boolean(formik.errors.max_players)
-              }
-              helperText={
-                formik.touched.max_players && formik.errors.max_players
-              }
-            />
-            <TextField
-              fullWidth
-              id="games"
-              name="games"
-              label="Cantidad de juegos*"
-              type="number"
-              value={formik.values.games}
-              onChange={formik.handleChange}
-              error={formik.touched.games && Boolean(formik.errors.games)}
-              helperText={formik.touched.games && formik.errors.games}
-            />
-            <TextField
-              fullWidth
-              id="rounds"
-              name="rounds"
-              label="Cantidad de rondas*"
-              type="number"
-              value={formik.values.rounds}
-              onChange={formik.handleChange}
-              error={formik.touched.rounds && Boolean(formik.errors.rounds)}
-              helperText={formik.touched.rounds && formik.errors.rounds}
-            />
-            <TextField
-              fullWidth
-              id="password"
-              name="password"
-              label="Contraseña de partida"
-              type="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-            <TextField
-              fullWidth
-              id="confirm_password"
-              name="confirm_password"
-              label="Confirmar contraseña de partida"
-              type="password"
-              value={formik.values.confirm_password}
-              onChange={formik.handleChange}
-              error={
-                formik.touched.confirm_password &&
-                Boolean(formik.errors.confirm_password)
-              }
-              helperText={
-                formik.touched.confirm_password &&
-                formik.errors.confirm_password
-              }
-            />
-            <RobotsSelect
-              select
-              value={formik.values.robot_id}
-              id="robot_id"
-              name="robot_id"
-              label="Elegir Robot*"
-              onChange={formik.handleChange}
-              inputProps={{
-                name: "robot_id",
-                id: "robot-s",
-              }}
-              error={formik.touched.robot_id && Boolean(formik.errors.robot_id)}
-              helperText={formik.touched.robot_id && formik.errors.robot_id}
-            />
-          </Stack>
-        </CardContent>
-        <CardActions sx={{ padding: 2 }}>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            id="createMatchSubmitButton"
-          >
-            Crear
-          </Button>
-        </CardActions>
-        {failure && (
-          <Alert severity="error">
-            <AlertTitle>
-              No se pudo crear la partida por un error en el servidor.
-            </AlertTitle>
-          </Alert>
-        )}
-      </form>
-    </Card>
+    <Grid container justifyContent="center">
+      <Grid item xs={12} lg={5}>
+        <Card variant="outlined" sx={{ width: "100%" }}>
+          <form onSubmit={formik.handleSubmit}>
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                textAlign="center"
+              >
+                Crear Partida
+              </Typography>
+              <Stack spacing={2}>
+                <TextField
+                  fullWidth
+                  id="name"
+                  name="name"
+                  label="Nombre de la partida*"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  error={formik.touched.name && Boolean(formik.errors.name)}
+                  helperText={formik.touched.name && formik.errors.name}
+                />
+                <TextField
+                  fullWidth
+                  id="min_players"
+                  name="min_players"
+                  label="Cantidad mínima de jugadores*"
+                  type="number"
+                  value={formik.values.min_players}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.min_players &&
+                    Boolean(formik.errors.min_players)
+                  }
+                  helperText={
+                    formik.touched.min_players && formik.errors.min_players
+                  }
+                />
+                <TextField
+                  fullWidth
+                  id="max_players"
+                  name="max_players"
+                  label="Cantidad máxima de jugadores*"
+                  type="number"
+                  value={formik.values.max_players}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.max_players &&
+                    Boolean(formik.errors.max_players)
+                  }
+                  helperText={
+                    formik.touched.max_players && formik.errors.max_players
+                  }
+                />
+                <TextField
+                  fullWidth
+                  id="games"
+                  name="games"
+                  label="Cantidad de juegos*"
+                  type="number"
+                  value={formik.values.games}
+                  onChange={formik.handleChange}
+                  error={formik.touched.games && Boolean(formik.errors.games)}
+                  helperText={formik.touched.games && formik.errors.games}
+                />
+                <TextField
+                  fullWidth
+                  id="rounds"
+                  name="rounds"
+                  label="Cantidad de rondas*"
+                  type="number"
+                  value={formik.values.rounds}
+                  onChange={formik.handleChange}
+                  error={formik.touched.rounds && Boolean(formik.errors.rounds)}
+                  helperText={formik.touched.rounds && formik.errors.rounds}
+                />
+                <TextField
+                  fullWidth
+                  id="password"
+                  name="password"
+                  label="Contraseña de partida"
+                  type="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+                <TextField
+                  fullWidth
+                  id="confirm_password"
+                  name="confirm_password"
+                  label="Confirmar contraseña de partida"
+                  type="password"
+                  value={formik.values.confirm_password}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.confirm_password &&
+                    Boolean(formik.errors.confirm_password)
+                  }
+                  helperText={
+                    formik.touched.confirm_password &&
+                    formik.errors.confirm_password
+                  }
+                />
+                <RobotsSelect
+                  select
+                  value={formik.values.robot_id}
+                  id="robot_id"
+                  name="robot_id"
+                  label="Elegir Robot*"
+                  onChange={formik.handleChange}
+                  inputProps={{
+                    name: "robot_id",
+                    id: "robot-s",
+                  }}
+                  error={
+                    formik.touched.robot_id && Boolean(formik.errors.robot_id)
+                  }
+                  helperText={formik.touched.robot_id && formik.errors.robot_id}
+                />
+              </Stack>
+            </CardContent>
+            <CardActions sx={{ padding: 2 }}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                id="createMatchSubmitButton"
+              >
+                Crear
+              </Button>
+            </CardActions>
+            {failure && (
+              <Alert severity="error">
+                <AlertTitle>
+                  No se pudo crear la partida por un error en el servidor.
+                </AlertTitle>
+              </Alert>
+            )}
+          </form>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
--- src/utils/AlertMessage.js
+++ src/utils/AlertMessage.js
@@ -1,13 +1,25 @@
 import { Snackbar, Alert } from "@mui/material"
+import React, { useState } from "react"
 
 export const SuccessMessage = ({ message }) => {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+  
+    setOpen(false);
+  };
+
   return (
     <Snackbar
-      open={true}
+      open={open}
       autoHideDuration={6000}
+      onClose={handleClose}
       anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
     >
-      <Alert severity="success" sx={{ width: "100%" }}>
+      <Alert severity="success" sx={{ width: "100%" }} onClose={handleClose}>
         {message}
       </Alert>
     </Snackbar>
