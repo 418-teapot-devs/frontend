25f9428125bbca6935ccab9591c6b5c2bc714243
Francesco Silvetti <francescosilvetti8@gmail.com>
2022-11-20 12:40:13 -0300

    Add recover password test

---
+++ src/features/user/RecoverPassword.test.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import { renderWithProviders } from "../../utils/testUtils"
+import RecoverPassword from "./RecoverPassword"
+
+test("should show ResetPasswordForm", async () => {
+  // FML
+  const window_location = window.location
+  delete window.location
+  window.location = { search: "?token=somevalidresettoken" }
+
+  renderWithProviders(<RecoverPassword />)
+
+  expect(await screen.findByTestId("reset-password-form")).toBeInTheDocument()
+
+  window.location = window_location
+})
+
+test("should show RecoverPasswordForm", async () => {
+  renderWithProviders(<RecoverPassword />)
+
+  expect(await screen.findByTestId("recover-password-form")).toBeInTheDocument()
+})
